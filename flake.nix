64c9af5cdfe2bc92891e195641cd61e5a8e3a6c5
ncaq <ncaq@ncaq.net>
2025-06-16 18:44:46 +0900

    chore: add Nix flake setup
    - Add .envrc to source local environment and use flake configuration
    - Add flake.nix and flake.lock for reproducible Nix development
    - Configure tooling for Haskell, formatting, and Nix environment
    - Update README.md to ensure new line at end of file

---
+++ flake.nix
@@ -0,0 +1,95 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    haskellNix = {
+      url = "github:input-output-hk/haskell.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+      haskellNix,
+    }:
+    let
+      ghc-version = "ghc9101";
+      overlays = [
+        haskellNix.overlay
+        (
+          final: _prev:
+          let
+            # haskell.nixのtoolsで参照されるhaskell-language-server。
+            tool-haskell-language-server =
+              final.haskell-nix.tool ghc-version "haskell-language-server"
+                "latest";
+          in
+          {
+            # nixpkgsで普通にインストールされるfourmoluはhaskell-language-serverのものと違うので上書きして合わせる。
+            inherit (tool-haskell-language-server.project.hsPkgs.fourmolu.components.exes) fourmolu;
+          }
+        )
+      ];
+    in
+    flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-linux ] (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          inherit (haskellNix) config;
+        };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs (_: {
+          projectRootFile = "flake.nix";
+          programs = {
+            cabal-gild.enable = true;
+            deadnix.enable = true;
+            hlint.enable = true;
+            nixfmt.enable = true;
+            prettier.enable = true;
+            shellcheck.enable = true;
+            shfmt.enable = true;
+            statix.enable = true;
+
+            fourmolu = {
+              enable = true;
+              package = pkgs.fourmolu;
+            };
+          };
+        });
+      in
+      {
+        checks = {
+          formatting = treefmtEval.config.build.check self;
+        };
+        formatter = treefmtEval.config.build.wrapper;
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cabal-install
+            ];
+          };
+        };
+      }
+    );
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+  };
+}
