ce9e2a6a054ab4eb0add21f1e532079f92f1f2eb
ncaq <ncaq@ncaq.net>
2025-06-16 19:11:10 +0900

    build: add kaisui cachix for binary cache and CI auth integration

--- flake.nix
+++ flake.nix
@@ -87,11 +87,13 @@
       "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
       "https://cache.iog.io"
+      "https://kaisui.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "kaisui.cachix.org-1:4r6+ZZtsY0YS5PkH2k9JorlwjC00vLiUzHNMxbeqBwM="
     ];
   };
 }
