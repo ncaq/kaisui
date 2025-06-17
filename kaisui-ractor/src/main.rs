d726f244a89435d467ef3371cbdda53c1167cb67
ncaq <ncaq@ncaq.net>
2025-06-17 17:17:23 +0900

    feat: change Rust library from xtra to ractor
    xtraがTCPとかに対応していなかったのでとりあえず変更。

---
+++ kaisui-ractor/src/main.rs
@@ -0,0 +1,4 @@
+#[tokio::main]
+async fn main() {
+    println!("kaisui-ractor: Testing ractor actor library");
+}
--- kaisui-xtra/src/main.rs
+++
@@ -1,4 +0,0 @@
-#[tokio::main]
-async fn main() {
-    println!("kaisui-xtra: Testing xtra actor library");
-}
