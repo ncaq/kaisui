d12f51ac4c6a33d3d3c2e6acb415b7664993b886
ncaq <ncaq@ncaq.net>
2025-06-20 00:53:49 +0900

    test: simplify KaisuiSpec by removing unnecessary error handling
    - Directly use createTransport and closeTransport without matching on result
    - Remove expectationFailure logic since createTransport no longer returns Either

--- network-transport-kaisui/test/Network/Transport/KaisuiSpec.hs
+++ network-transport-kaisui/test/Network/Transport/KaisuiSpec.hs
@@ -1,15 +1,12 @@
 module Network.Transport.KaisuiSpec (spec) where
 
-import Network.Transport.Kaisui
+import Network.Transport (closeTransport)
+import Network.Transport.Kaisui (createTransport)
 import RIO
 import Test.Syd
 
 spec :: Spec
 spec = describe "Network.Transport.Kaisui" $ do
   it "should create and close transport" $ do
-    result <- createTransport "127.0.0.1" "8080"
-    case result of
-      Left err -> expectationFailure $ "Failed to create transport: " <> show err
-      Right transport -> do
-        closeTransport transport
-        pure ()
+    transport <- createTransport "127.0.0.1" "8080"
+    closeTransport transport
